861e6b3a39afe64084d2eb594813b5a3915e32c3
maria <maria.cabeza.castillo@gmail.com>
2015-04-15 12:28:36 +0200

    finalizado el boton siguiente aunque aun con algunos errores

--- LectorDePaginas/src/LectorLibroUno.java
+++ LectorDePaginas/src/LectorLibroUno.java
@@ -20,7 +20,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-
 public class LectorLibroUno extends JFrame {
 
 	protected JPanel contentPane;
@@ -28,11 +27,15 @@ public class LectorLibroUno extends JFrame {
 	protected JButton adelante;
 	protected JTextArea txtrLaMejorHistoria;
 	protected JButton atras;
+
+	File archivo = new File("JuegoTronos.txt");
+	FileReader flujo = null;
+	BufferedReader bufer = null;
+	int contador = 0;
 	
-	File archivo=new File("JuegoTronos.txt");
-	FileReader flujo=null;
-	BufferedReader bufer=null;
-	char[] lector=new char[110];
+
+	char[] lector = new char[800];
+	char caracter;
 
 	/**
 	 * Launch the application.
@@ -54,66 +57,82 @@ public class LectorLibroUno extends JFrame {
 	 * Create the frame.
 	 */
 	public LectorLibroUno() {
-		setResizable(false);
+		//setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 265, 484);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.CENTER);
 		panel.setLayout(new GridLayout(0, 1, 0, 0));
-		
+
 		txtrLaMejorHistoria = new JTextArea();
-		txtrLaMejorHistoria.setFont(new Font("Monospaced", Font.PLAIN, 17));
+		txtrLaMejorHistoria.setFont(new Font("Monospaced", Font.PLAIN, 12));
 		panel.add(txtrLaMejorHistoria);
-//		Hace un salto de linea si detecta que llega al borde del area
+		// Hace un salto de linea si detecta que llega al borde del area
 		txtrLaMejorHistoria.setLineWrap(true);
-//		detecta si es una palabra o no
+		// detecta si es una palabra o no
 		txtrLaMejorHistoria.setWrapStyleWord(true);
 		System.out.println(txtrLaMejorHistoria.getFont().getSize());
 		System.out.println(txtrLaMejorHistoria.getHeight());
-		
+
 		JPanel panel_1 = new JPanel();
 		contentPane.add(panel_1, BorderLayout.SOUTH);
-		
+
 		btnMarca = new JButton("marca");
 		btnMarca.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		panel_1.add(btnMarca);
-		
+
 		adelante = new JButton("adelante");
 		adelante.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-//				System.out.println("el tamaño "+txtrLaMejorHistoria.getHeight());
-//				System.out.println("lo ocupado actualmente "+txtrLaMejorHistoria.getPreferredSize().getHeight());
-//				txtrLaMejorHistoria.append("la primera cosa que se escribee es la mejor y ademas quiero ocupar mas espacio en la ventana"
-//						+ "ahora voy a escribir una linea mucho mas larga con la intencion de que sobrepase no solo el tamaño actual de la ventana si no que"
-//						+ "ademas queremos sobrepasar con mucho lo que seria una pagina para poder probar que esta funcionalidad en realidad hace lo que"
-//						+ " tiene que hacer");
 				
-				if(archivo.exists())
+				// txtrLaMejorHistoria.append("la primera cosa que se escribee es la mejor y ademas quiero ocupar mas espacio en la ventana"
+				// +
+				// "ahora voy a escribir una linea mucho mas larga con la intencion de que sobrepase no solo el tamaño actual de la ventana si no que"
+				// +
+				// "ademas queremos sobrepasar con mucho lo que seria una pagina para poder probar que esta funcionalidad en realidad hace lo que"
+				// + " tiene que hacer");
+
+				if (archivo.exists())
 					try {
-						
-						bufer.reset();
-						System.out.println("empieza la lectura");
-						bufer.read(lector);
-						txtrLaMejorHistoria.setText(new String(lector));
+						if (contador == 0) {
+							flujo = new FileReader(archivo);
+							bufer = new BufferedReader(flujo);
+							// bufer.mark(100);
+							// bufer.reset();
+						}
+
+						txtrLaMejorHistoria.setText(" ");
+						// Leyendo byte a byte
+						do{
+							caracter = (char) bufer.read();
+							txtrLaMejorHistoria.append(caracter + "");
+
+						}while(txtrLaMejorHistoria.getHeight() > txtrLaMejorHistoria.getPreferredSize().getHeight());
+						System.out.println("el tamaño " + txtrLaMejorHistoria.getHeight());
+						System.out.println("lo ocupado actualmente " + txtrLaMejorHistoria.getPreferredSize().getHeight());
+						// txtrLaMejorHistoria.setText(new String(lector));
+
+						// bufer.read(lector);
+						// txtrLaMejorHistoria.setText(new String(lector));
 					} catch (FileNotFoundException e1) {
 						e1.printStackTrace();
-					}catch (IOException e1) {
+					} catch (IOException e1) {
 						e1.printStackTrace();
 					}
+				contador++;
 			}
-			
-			
+
 		});
 		panel_1.add(adelante);
-		
+
 		atras = new JButton("atras");
 		atras.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
